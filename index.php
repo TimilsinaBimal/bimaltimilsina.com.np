5aa52efc4c267820fb3f146c844928d690e8b498
TimilsinaBimal <timilsina.bml05@gmail.com>
2019-11-01 10:05:47 +0545

    Initial Commit

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bimal Timilsina</title>
+    <link rel="shortcut icon" href="logo.png" type="image/x-icon">
+
+    <!-- CSS FILES -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- <nav id="nav" class="navbar navbar-expand-lg bg-transparent bgc fixed-top">
+        <div class="container wave">
+            <span>
+                <a class="navbar-brand" href="">
+                    <h4><b>Bimal Timilsina</b> </h4>
+                </a>
+            </span>
+            <button class="navbar-toggler first-button" type="button" data-toggle="collapse"
+                data-target="#collapse_target" aria-controls="collapse_target" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <div class="animated-icon"><span></span><span></span><span></span></div>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="collapse_target">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a id="navItems" href="#" class="nav-link active">Home</a></li>
+                    <li class="nav-item"><a id="navItems" data-scroll href="#" class="nav-link">About</a></li>
+                    <li class="nav-item"><a id="navItems" data-scroll href="#" class="nav-link">Blog</a></li>
+                    <li class="nav-item"><a id="navItems" data-scroll href="#" class="nav-link">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav> -->
+    <div class="header-color"></div>
+    <div class="intro">
+        <div class="container-fluid">
+            <div class="row justify-content-center">
+                <div class="col-lg-8 col-md-10 col-sm-11 myCard">
+                    <div class="row justify-content-center">
+                        <div class="col-md-6 col-lg-6 col-sm-10">
+                            <img class="img-fluid" src="bimal.jpg" alt="Bimal Timilsina">
+                        </div>
+                        <div class="col-md-6 col-lg-6 col-sm-10">
+                            <span>Hello Everybody, I am</span>
+                            <h1>bimal</h1>
+                            <h1>Timilsina</h1>
+                            <h6>Undergraduate Student</h6>
+                            <p>Get going. Move forward. Aim High. Plan a takeoff. Don't just sit on the runway and hope
+                                someone will come along and push the airplane. It simply won't happen. Change your
+                                attitude and gain some altitude.</p>
+                            <!-- <ul class="more-info"> -->
+                            <li><i class="fa fa-calendar"></i>30 June, 2000</li>
+                            <li><i class="fa fa-phone"></i>+977-9867232628</li>
+                            <li><i class="fa fa-envelope"></i>timilsina.bml05@gmail.com</li>
+                            <li><i class="fa fa-home"></i>Butwal, Rupandehi</li>
+                            <!-- </ul> -->
+                            <div class="social-btns">
+                                <a class="btns linkedin" href="https://linkedin.com/in/TimilsinaBimal"><i
+                                        class="fa fa-linkedin"></i></a>
+                                <a class="btns github" href="https://github.com/TimilsinaBimal"><i
+                                        class="fa fa-github"></i></a>
+                                <a class="btns instagram" href="https://instagram.com/__bimal_/"><i
+                                        class="fa fa-instagram"></i></a>
+                                <a class="btns email" href="mailto:timilsina.bml05@gmail.com"><i
+                                        class="fa fa-envelope"></i></a>
+                                <a class="btns wordpress" href="https://timilsinabimal.wordpress.com"><i
+                                        class="fa fa-wordpress"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- JS Files -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
